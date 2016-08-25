631f82659c927428a328dfddfdb27137ca21b80e
Amaresh Chauhan <amr.chauhan11@gmail.com>
2016-08-25 09:01:59 +0530

    first

---
+++ test_app.rb
@@ -0,0 +1,63 @@
+require 'sinatra' 
+require 'slim'
+require 'data_mapper'
+
+get '/' do 
+  @lists = List.all(:order => [:name])
+  slim :index 
+end
+
+get '/:task' do
+  @task = params[:task]
+  slim :task
+end
+
+post '/:id' do
+  List.get(params[:id]).tasks.create params['task']
+  redirect to('/')
+end
+
+delete '/task/:id' do
+  Task.get(params[:id]).destroy
+  redirect to('/')
+end
+
+put '/task/:id' do
+  task = Task.get params[:id]
+  task.completed_at = task.completed_at.nil? ? Time.now : nil
+  task.save
+  redirect to('/')
+end
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+
+class Task
+  include DataMapper::Resource
+  property :id,           Serial
+  property :name,         String, :required => true
+  property :completed_at, DateTime
+  belongs_to :list
+end
+
+class List
+  include DataMapper::Resource
+  property :id, Serial
+  property :name, String, :required => true
+  has n, :tasks, :constraint => :destroy
+  end
+
+DataMapper.finalize
+
+post '/new/list' do
+  List.create params['list']
+  redirect to('/')
+end
+
+delete '/list/:id' do
+  List.get(params[:id]).destroy
+  redirect to('/')
+end
+
+get '/styles.css' do 
+  scss :styles
+end
